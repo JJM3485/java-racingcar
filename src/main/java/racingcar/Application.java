9de8d6bb24ae0ba3993173b3c51576fe46255e3c
조재민 <JJM3485>
2025-09-18 10:49:18 +0900

    feat : 자동차 이름, 횟수 받아 라운드 실행 기능  구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,43 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String carString = Console.readLine();
+        String[] carNames = carString.split(",");
+
+        // 시도 횟수 입력
+        System.out.println("시도할 횟수를 입력하세요.");
+        String input = Console.readLine();
+        int rounds = Integer.parseInt(input);
+
+        final int START_NUMBER = 0;
+        final int END_NUMBER = 9;
+
+        // 자동차 전진 거리 저장 배열
+        int[] positions = new int[carNames.length];
+
+        System.out.println("\n실행 결과");
+
+        // 라운드 수만큼 반복
+        for (int i = 0; i < rounds; i++) {
+            for (int j = 0; j < carNames.length; j++) {
+                int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+                if (randomNumber >= 4) {
+                    positions[j]++;
+                }
+            }
+            // 각 라운드 결과 출력
+            for (int j = 0; j < carNames.length; j++) {
+                System.out.println(carNames[j] + " : " + "-".repeat(positions[j]));
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
